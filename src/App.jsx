f3a482f412f74f76fc576ba278a9d948360f85cb
xmarimarquesh <marimrquesh39@gmail.com>
2024-09-16 16:00:16 -0300

    Projetos OK

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <a href="">Contato</a>
       </div>
       <main>
-        <h1>Seja bem vindo ao meu portfólio Profissional!</h1>
+        <h1 className={style.titulo}>Seja bem vindo ao meu portfólio Profissional!</h1>
         <section className={style.apresentacao}>
               <div className={style.texto_apresentacao}>
                   <h2>Olá! me chamo Mariana Marques</h2>
@@ -30,18 +30,38 @@ function App() {
               <img className={style.eu} src="../src/assets/img/eu2.png" alt=""/>
           </section>
 
-        <section className={style.experiencias}>
-        <h1>Experiências</h1>
-            <div className={style.experiencia}>
-                <img src="https://yt3.googleusercontent.com/nk7VSREl8PCdRZhGcLkggJ5vacaxnTxW5BWbiflQOoMjLcM6Zg4LdZJYPNmQOH0a48Zbtd8L-Q=s176-c-k-c0x00ffffff-no-rj-mo" alt="bosch"/>
-                <div>
-                    <h2>Jovem Aprendiz em</h2>
-                    <h1>Soluções Digitais</h1>
-                    <h3>Robert Bosch</h3>
-                    <h4>05/02/2024 - 05/07/2025</h4>
+          <section className={style.experiencias}>
+            <h1>Experiências</h1>
+              <div className={style.experiencia}>
+                  <img src="https://yt3.googleusercontent.com/nk7VSREl8PCdRZhGcLkggJ5vacaxnTxW5BWbiflQOoMjLcM6Zg4LdZJYPNmQOH0a48Zbtd8L-Q=s176-c-k-c0x00ffffff-no-rj-mo" alt="bosch"/>
+                  <div>
+                      <h2>Jovem Aprendiz em</h2>
+                      <h1>Soluções Digitais</h1>
+                      <h3>Robert Bosch</h3>
+                      <h4>05/02/2024 - 05/07/2025</h4>
+                  </div>
+              </div>
+          </section>
+
+          <section className={style.projetos}>
+            <h1>Projetos</h1>
+            <div className={style.projeto}>
+                <div className={style.texto_projeto}>
+                    <h2>Coffes Garden</h2>
+                    <h3>Tecnologias: PHP, VsCode, MySQL, AJAX, JavaScript, HTML</h3>
+                    <p>Um site de uma cafeteria e floricultura 100% funcional e aplicável. Conta com uma interface para o cliente e outra para o administrador.</p>
+                </div>
+                <iframe src="../src/assets/img/videos/0809.mp4"></iframe>
+            </div>
+            <div className={style.projeto}>
+                <iframe src="../src/assets/img/videos/termo.mp4"></iframe>
+                <div className={style.texto_projeto}>
+                    <h2>TERMO</h2>
+                    <h3>Tecnologias: Linguagem C, VsCode</h3>
+                    <p>O termo é um jogo de advinhação de palavras, tendo dois modos, você deve acertar a palavra com base nas cores verde, vermelho e amarelo.</p>
                 </div>
             </div>
-        </section>
+          </section>
         </main>
     </>
   )
