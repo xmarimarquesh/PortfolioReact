3a8606bde7a6176af9fd106c8d3d63fe9e77b969
xmarimarquesh <marimrquesh39@gmail.com>
2024-09-16 14:45:45 -0300

    Menu OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.module.css'
+import style from './App.module.css'
 
 function App() {
 
   return (
     <>
-      
+      <div className={style.Menu}>
+        <a href="">Início</a>
+        <a href="">Experiências</a>
+        <a href="">Projetos</a>
+        <a href="">Contato</a>
+      </div>
     </>
   )
 }
